009b6d07ea594f5f936e87ed2d419630d0c274c5
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-05 14:39:52 -0400

    implemented setupInput and handleUserInput function

--- play.js
+++ play.js
@@ -6,6 +6,23 @@ const net = require("net");
 
 
 console.log("Connecting ...");
+// setup interface to handle user input from stdin
 
-connect()
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+connect();
+setupInput();
