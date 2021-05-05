02e7690ce5f24d3588d010e674af901acddcc565
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-05 14:44:49 -0400

    refactored code to seperate file play.js and input.js for input functionality

--- play.js
+++ play.js
@@ -1,4 +1,5 @@
 const { connect } = require('../snake-client/client');
+const {setupInput} = require('../snake-client/input');
 const net = require("net");
 
 
@@ -6,23 +7,6 @@ const net = require("net");
 
 
 console.log("Connecting ...");
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 connect();
 setupInput();
