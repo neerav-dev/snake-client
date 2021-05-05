9c0cad7afbd2922bcbcf78c31237a8bc6101219a
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-05 12:59:45 -0400

    refactored code to seperate file client.js and play.js

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.222.131',// IP address here,
+    port: 50542// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log('you ded cuz you idled');
+  });
+
+  return conn;
+};
+
+
+
+// console.log("Connecting ...");
+// connect();
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,22 +1,7 @@
+const { connect } = require('../snake-client/client');
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.222.131',// IP address here,
-    port: 50542// PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log('you ded cuz you idled');
-  });
-
-  return conn;
-};
 
 
 
